77264c3f11f834f4f3e63fbc784d3dcf82540fdb
Gennady Kovalev <gik@bigur.ru>
2018-08-01 01:56:20 +0300

    Добавлен модуль amqp

--- setup.py
+++ setup.py
@@ -28,7 +28,9 @@ setup(
     keywords=['bigur', 'auth'],
 
     # Библиотека
-    packages=['bigur/auth', 'bigur/auth/migration'],
+    packages=['bigur/auth',
+              'bigur/auth/amqp',
+              'bigur/auth/migration'],
 
     # Файлы настроек
     data_files=[('/etc/bigur', ['conf/auth.conf'])],
