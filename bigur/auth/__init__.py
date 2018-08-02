aa14d5be7d290e7d4c72aef168f4bccc1704b582
Gennady Kovalev <gik@bigur.ru>
2018-08-02 12:55:22 +0300

    Удалены лишние эвенты

--- bigur/auth/__init__.py
+++ bigur/auth/__init__.py
@@ -2,11 +2,4 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2018 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
-__version__ = '3.0.2'
-
-from bigur.rx import Subject
-
-
-on_migration = Subject()
-
-on_startup = Subject()
+__version__ = '1.0.0'
