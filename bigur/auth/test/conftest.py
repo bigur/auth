521f305de440e69fdbdb03038edb180f9cbd73ea
Gennady Kovalev <gik@bigur.ru>
2019-01-22 22:08:44 +0300

    Фикстуры для БД

--- bigur/auth/test/conftest.py
+++ bigur/auth/test/conftest.py
@@ -2,12 +2,16 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
+from os import environ
 from os.path import dirname, normpath
 
 from aiohttp_jinja2 import setup as jinja_setup
 from aiohttp.web import Application, view
 from jinja2 import FileSystemLoader
-from pytest import fixture
+from pytest import fixture, mark
+
+from bigur.store import db
+from bigur.utils import config
 
 from bigur.auth.handlers import AuthorizationHandler
 from bigur.auth.middlewares import authenticate
@@ -39,3 +43,20 @@ def cli(loop, aiohttp_client):
     jinja_setup(app, loader=FileSystemLoader(templates))
 
     return loop.run_until_complete(aiohttp_client(app))
+
+
+@fixture
+async def database():
+    '''Доступ к базе данных.'''
+    conf = config.get_object()
+    if not conf.has_section('general'):
+        conf.add_section('general')
+    conf.set('general', 'database_url', environ.get('BIGUR_TEST_DB'))
+    db._db = None
+    for collection in await db.list_collection_names():
+        await db.drop_collection(collection)
+    yield db
+
+
+mark.db_configured = mark.skipif(environ.get('BIGUR_TEST_DB') is None,
+                                 reason='Не настроена база данных')
