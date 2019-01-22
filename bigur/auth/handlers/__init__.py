72608a3e73d646eb71168c46c86be1ed27218189
Gennady Kovalev <gik@bigur.ru>
2019-01-22 20:17:02 +0300

    Убран LoginFormHandler

--- bigur/auth/handlers/__init__.py
+++ bigur/auth/handlers/__init__.py
@@ -2,6 +2,7 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2018 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
+# flake8: noqa
+
 from .authorize import AuthorizationHandler
-from .login_form import LoginFormHandler
 from .root import RootHandler
