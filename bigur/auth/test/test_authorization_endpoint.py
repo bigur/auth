86f7611b2456000856f11e79e3df995644aca45d
Gennady Kovalev <gik@bigur.ru>
2019-01-22 20:18:49 +0300

    Убраны фикстуры

--- bigur/auth/test/test_authorization_endpoint.py
+++ bigur/auth/test/test_authorization_endpoint.py
@@ -2,36 +2,12 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
-from aiohttp.web import Application, view
-from aiohttp.pytest_plugin import aiohttp_client  # noqa: F401
-from pytest import fixture, mark
+from pytest import mark
 
 from bigur.store import UnitOfWork
 from bigur.store.test.fixtures import configured, database  # noqa: F401
 
 from bigur.auth.model import User, Client
-from bigur.auth.handlers import AuthorizationHandler
-
-
-@fixture
-def loop(event_loop):
-    return event_loop
-
-
-@fixture
-def debug(caplog):
-    '''Отладка тестов.'''
-    from logging import DEBUG
-    caplog.set_level(DEBUG, logger='bigur.auth')
-
-
-@fixture  # noqa: F811
-def cli(loop, aiohttp_client):
-    app = Application(loop=loop)
-    app.add_routes([
-        view('/authorize', AuthorizationHandler)
-    ])
-    return loop.run_until_complete(aiohttp_client(app))
 
 
 class TestAuthorizationEndpoint(object):
