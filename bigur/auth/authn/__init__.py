f2dc1664c70dc14cff914ff19aeb589e984e08ef
Gennady Kovalev <gik@bigur.ru>
2019-01-25 22:09:00 +0300

    Add authenticate_resource_owner()

--- bigur/auth/authn/__init__.py
+++ bigur/auth/authn/__init__.py
@@ -2,8 +2,8 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
+# flake8: noqa
+
+from .base import authenticate_resource_owner
+
 from .user_pass import UserPass
-
-
-def choose_authn(request):
-    return UserPass()
