528a1a6a01720f8854e0d458cb214a177a42fc99
Gennady Kovalev <gik@bigur.ru>
2019-02-15 14:44:21 +0300

    Remove migration module

--- bigur/auth/migration/__init__.py
+++
@@ -1,5 +0,0 @@
-__author__ = 'Gennady Kovalev <gik@bigur.ru>'
-__copyright__ = '(c) 2016-2018 Business group for development management'
-__licence__ = 'For license information see LICENSE'
-
-from . import init
