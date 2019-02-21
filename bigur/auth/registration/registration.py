be3904921662270a4fe8291542da1b4d74b5c839
Gennady Kovalev <gik@bigur.ru>
2019-02-21 20:38:01 +0300

    Empty registratin handler

--- bigur/auth/registration/registration.py
+++ bigur/auth/registration/registration.py
@@ -2,5 +2,8 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
-# flake8: noqa
-from .registration import Registration
+from aiohttp.web import View
+
+
+class Registration(View):
+    pass
