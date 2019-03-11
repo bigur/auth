626b0ff612113b832dde98b20848ebbfc83c6842
Gennady Kovalev <gik@bigur.ru>
2019-03-11 18:11:48 +0300

    Load rsa keys for JWT

--- bigur/auth/test/conftest.py
+++ bigur/auth/test/conftest.py
@@ -8,6 +8,8 @@ from os.path import dirname, normpath
 from aiohttp import CookieJar
 from aiohttp.web import Application, view
 from aiohttp_jinja2 import setup as jinja_setup
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa
 from jinja2 import FileSystemLoader
 from kaptan import Kaptan
 from pytest import fixture
@@ -63,7 +65,10 @@ def app():
     })
 
     app['cookie_key'] = urandom(32)
-
+    app['jwt_keys'] = [
+        rsa.generate_private_key(
+            public_exponent=65537, key_size=2048, backend=default_backend())
+    ]
     app['store'] = Memory()
 
     templates = normpath(dirname(__file__) + '../../../../templates')
