403b8e28738b11f58e5c5db960fade7ac09c74f8
Gennady Kovalev <gik@bigur.ru>
2019-03-27 20:19:22 +0300

    Add endpoint class

--- bigur/auth/handler/oauth2/authorize.py
+++ bigur/auth/handler/oauth2/authorize.py
@@ -2,14 +2,10 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Development management business group'
 __licence__ = 'For license information see LICENSE'
 
-
-
-
-
-
 from bigur.auth.handler.base import OAuth2Handler
-
+from bigur.auth.oauth2.endpoint import AuthorizationEndpoint
 
 
 class AuthorizationHandler(OAuth2Handler):
-    pass
+
+    __endpoint__ = AuthorizationEndpoint
