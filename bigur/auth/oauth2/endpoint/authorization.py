2e9582803a1b0dd3df5c9d1037a4e1e8183cdb96
Gennady Kovalev <gik@bigur.ru>
2019-04-02 21:41:16 +0300

    Response type parameter stream

--- bigur/auth/oauth2/endpoint/authorization.py
+++ bigur/auth/oauth2/endpoint/authorization.py
@@ -4,22 +4,33 @@ __licence__ = 'For license information see LICENSE'
 
 from dataclasses import dataclass
 from logging import getLogger
-from typing import Dict
 
-from aiohttp.web import Request
+from multidict import MultiDictProxy
 
-from bigur.rx import Subject, Observer
+from bigur.rx import Subject, ObserverBase, ObservableBase, operators as op
 
 from bigur.auth.model import User
 from bigur.auth.oauth2.exceptions import InvalidRequest
-from bigur.auth.oauth2.request import OAuth2Request
+from bigur.auth.oauth2.request import OAuth2Request, create_request
 from bigur.auth.oauth2.response import OAuth2Response
+from bigur.auth.oauth2.validators import (
+    validate_client_id, authenticate_client, validate_redirect_uri)
 
 logger = getLogger(__name__)
 
 
 @dataclass
-class AuthorizationRequest(OAuth2Request):
+class AuthCodeRequest(OAuth2Request):
+    pass
+
+
+@dataclass
+class AccessTokenRequest(OAuth2Request):
+    pass
+
+
+@dataclass
+class InvalidResponseTypeRequest(OAuth2Request):
     pass
 
 
@@ -28,43 +39,83 @@ class AuthorizationResponse(OAuth2Response):
     pass
 
 
-async def create_oauth2_request(request: Request):
-    logger.debug('Creating request from POST method')
-    # XXX: ignore unneeded parameters
-    try:
-        kwargs = await request.post()
-        request['oauth2_request'] = AuthorizationRequest(  # type: ignore
-            **(kwargs))
-    except TypeError as e:
-        raise InvalidRequest(str(e)[11:])
-    else:
-        return request
+class AuthorizationEndpoint(ObserverBase, ObservableBase):
 
-
-class AuthCodeEndpoint(Subject):
-
-    def __init__(self, owner: User, params: Dict):
+    def __init__(self, owner: User, params: MultiDictProxy):
         self._owner = owner
         self._params = params
+        self._observer: ObserverBase = None
         super().__init__()
 
-    async def subscribe(self, observer: Observer):
-        await super().subscribe(observer)
+    def authorization_code_stream(self, stream) -> ObservableBase:
+        pass
 
-        if 'response_type' not in self._params:
-            await self.on_error(InvalidRequest('No response_type parameter'))
-        else:
-            response_type = self._params['response_type']
-            if not isinstance(response_type, list):
-                response_type = {response_type}
-            else:
-                response_type = set(response_type)
+    def access_token_stream(self, stream) -> ObservableBase:
+        pass
 
+    def invalid_response_type_stream(self, stream,
+                                     description: str) -> ObservableBase:
+
+        def raise_invalid_request(request: OAuth2Request):
+            raise InvalidRequest(request, description)
+
+        return (stream
+                | op.map(validate_client_id)
+                | op.map(authenticate_client)
+                | op.map(validate_redirect_uri)
+                | op.map(raise_invalid_request))
+
+    async def _subscribe(self, observer: ObserverBase):
+        self._observer = observer
+
+        stream = Subject()
+
+        try:
+            response_types = self._params.getall('response_type')
+            if len(response_types) > 1:
+                raise ValueError('Multiple values in response_type '
+                                 'are not allowed')
+
+            response_type = response_types[0]
+
+            # Access core request
             if response_type == 'code':
-                pass
+                request_type = AuthCodeRequest
+                xs = self.authorization_code_stream(stream)
+
+            # Token request with implicit grant
             elif response_type == 'token':
-                pass
+                request_type = AccessTokenRequest
+                xs = self.access_token_stream(stream)
+
+            # Invalid response_type value
             else:
-                await self.on_error(
-                    InvalidRequest('Invalid response_type value'))
-        stream = AuthorizationCodeGrant()
+                raise ValueError('Invalid response_type value')
+
+        except (KeyError, ValueError) as e:
+            # Invalid response_type value. This exception we must
+            # raise with redirection to client's endpoint, but first
+            # of all we must check client and validate redirect_uri.
+            if isinstance(e, KeyError):
+                description = 'Missing response_type parameter'
+            else:
+                description = str(e)
+
+            request_type = InvalidResponseTypeRequest
+            xs = self.invalid_response_type_stream(stream, description)
+
+        request = await create_request(request_type, self._owner, self._params)
+
+        await xs.subscribe(self)
+
+        await stream.on_next(request)
+        await stream.on_completed()
+
+    async def on_next(self, response: OAuth2Response):
+        await self._observer.on_next(response)
+
+    async def on_error(self, error: Exception):
+        await self._observer.on_error(error)
+
+    async def on_completed(self):
+        await self._observer.on_completed()
