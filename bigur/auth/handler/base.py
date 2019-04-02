d68adf4be73881bafdc6673fec0ff645f9200d13
Gennady Kovalev <gik@bigur.ru>
2019-04-02 21:42:05 +0300

    Handle exceptions

--- bigur/auth/handler/base.py
+++ bigur/auth/handler/base.py
@@ -3,65 +3,33 @@ __copyright__ = '(c) 2016-2019 Development management business group'
 __licence__ = 'For license information see LICENSE'
 
 from logging import getLogger
-from typing import Dict, List, Type
-
-from aiohttp.web import Request, View
-
-from bigur.rx import ObserverBase
-
+from typing import Dict, List, Optional, Type
 from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 
+from aiohttp.web import Request, Response, View
 from aiohttp.web_exceptions import (HTTPBadRequest, HTTPInternalServerError,
                                     HTTPSeeOther)
 
-from bigur.rx import ObserverBase, Subject
-from bigur.rx import operators as op
+from bigur.rx import ObserverBase
 
 from bigur.auth.authn import authenticate_end_user
-from bigur.auth.oauth2.grant import implicit_grant
+from bigur.auth.oauth2.exceptions import (OAuth2FatalError, OAuth2RedirectError)
+from bigur.auth.oauth2.response import OAuth2Response
+from bigur.auth.utils import asdict
 
 logger = getLogger(__name__)
 
 
-class ResultObserver(ObserverBase[Request]):
+class ResultObserver(ObserverBase):
 
     def __init__(self, request: Request):
         self._request = request
-        self._responses = []
-        super().__init__()
+        self._responses: List[OAuth2Response] = []
 
-    async def on_next(self, request: Request) -> None:
-        params: Dict[str, str] = {}
-        for oauth2_response in request['oauth2_responses']:
-            params.update(asdict(oauth2_response))
+        self.response: Optional[Response] = None
 
-        if request['oauth2_request'].state:
-            params['state'] = request['oauth2_request'].state
-
-        fragment: Dict[str, str] = {}
-        query: Dict[str, str] = {}
-
-        logger.warning('Hardcoded response mode: fragment')
-        mode = 'fragment'
-        if mode == 'fragment':
-            fragment = params
-        elif mode == 'query':
-            query = params
-        else:
-            raise NotImplementedError('response mode not implemented')
-
-        url = urlparse(request['oauth2_request'].redirect_uri)
-        query.update(parse_qs(url.query))
-        fragment.update(parse_qs(url.fragment))
-
-        self.response = Response(
-            status=303,
-            headers={
-                'Location':
-                    urlunparse((url.scheme, url.netloc, url.path, url.params,
-                                urlencode(query, doseq=True),
-                                urlencode(fragment, doseq=True)))
-            })
+    async def on_next(self, response: OAuth2Response) -> None:
+        self._responses.append(response)
 
     async def on_error(self, error: Exception):
         if isinstance(error, OAuth2FatalError):
@@ -81,6 +49,36 @@ class ResultObserver(ObserverBase[Request]):
     async def on_completed(self):
         logger.debug('Request process finished')
 
+        params: Dict[str, str] = {}
+        for response in self._responses:
+            print(response)
+            params.update(asdict(response))
+
+        fragment: Dict[str, str] = {}
+        query: Dict[str, str] = {}
+
+        logger.warning('Hardcoded response mode: fragment')
+        mode = 'fragment'
+        if mode == 'fragment':
+            fragment = params
+        elif mode == 'query':
+            query = params
+        else:
+            raise NotImplementedError('response mode not implemented')
+
+        url = urlparse(self._request['params']['redirect_uri'])
+        query.update(parse_qs(url.query))
+        fragment.update(parse_qs(url.fragment))
+
+        self.response = Response(
+            status=303,
+            headers={
+                'Location':
+                    urlunparse((url.scheme, url.netloc, url.path, url.params,
+                                urlencode(query, doseq=True),
+                                urlencode(fragment, doseq=True)))
+            })
+
 
 class OAuth2Handler(View):
 
@@ -89,9 +87,13 @@ class OAuth2Handler(View):
     async def handle(self):
         request = self.request
         owner = await authenticate_end_user(request)
+
         stream = self.__endpoint__(owner=owner, params=request['params'])
+
         result = ResultObserver(request)
         await stream.subscribe(result)
+
+        return result.response
 
     async def get(self):
         self.request['params'] = self.request.query
