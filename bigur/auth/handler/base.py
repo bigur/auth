ab3ecc1a9ac6cfb6cd49eb8f3da6bea613240594
Gennady Kovalev <gik@bigur.ru>
2019-04-21 11:34:31 +0300

    Rewrite result observer

--- bigur/auth/handler/base.py
+++ bigur/auth/handler/base.py
@@ -3,17 +3,21 @@ __copyright__ = '(c) 2016-2019 Development management business group'
 __licence__ = 'For license information see LICENSE'
 
 from logging import getLogger
-from typing import Dict, List, Optional, Type
+from typing import Dict, Optional
 from urllib.parse import urlparse, urlunparse, parse_qs, urlencode
 
-from aiohttp.web import Request, Response, View
-from aiohttp.web_exceptions import (HTTPBadRequest, HTTPInternalServerError,
-                                    HTTPSeeOther)
+from aiohttp.web import Response, View
+from aiohttp.web_exceptions import (HTTPBadRequest, HTTPInternalServerError)
+
+from multidict import MultiDictProxy
 
 from bigur.rx import ObserverBase
 
 from bigur.auth.authn import authenticate_end_user
-from bigur.auth.oauth2.exceptions import (OAuth2FatalError, OAuth2RedirectError)
+from bigur.auth.oauth2.endpoint import Endpoint
+from bigur.auth.oauth2.exceptions import (OAuth2FatalError,
+                                          OAuth2RedirectionError)
+from bigur.auth.oauth2.request import OAuth2Request
 from bigur.auth.oauth2.response import OAuth2Response
 from bigur.auth.utils import asdict
 
@@ -22,55 +26,53 @@ logger = getLogger(__name__)
 
 class ResultObserver(ObserverBase):
 
-    def __init__(self, request: Request):
+    def __init__(self, request: OAuth2Request):
         self._request = request
-        self._responses: List[OAuth2Response] = []
+        self._response: Optional[OAuth2Response] = None
 
-        self.response: Optional[Response] = None
-
-    async def on_next(self, response: OAuth2Response) -> None:
-        self._responses.append(response)
-
-    async def on_error(self, error: Exception):
-        if isinstance(error, OAuth2FatalError):
-            logger.debug('Return HTTPBadRequest("%s")', str(error))
-            raise HTTPBadRequest(reason=str(error))
-
-        elif isinstance(error, OAuth2RedirectError):
-            # TODO: check oidc's response_mode parameter
-            # TODO: check prompt parameter
-            logger.debug('Redirect with error: %s', error.location)
-            raise HTTPSeeOther(error.location)
-
-        else:
-            logger.error('Internal server error', exc_info=error)
-            raise HTTPInternalServerError()
-
-    async def on_completed(self):
-        logger.debug('Request process finished')
-
+    def http_response(self):
+        request = self._request
+        response = self._response
         params: Dict[str, str] = {}
-        for response in self._responses:
-            print(response)
-            params.update(asdict(response))
 
         fragment: Dict[str, str] = {}
         query: Dict[str, str] = {}
 
-        logger.warning('Hardcoded response mode: fragment')
-        mode = 'fragment'
-        if mode == 'fragment':
-            fragment = params
-        elif mode == 'query':
-            query = params
-        else:
-            raise NotImplementedError('response mode not implemented')
+        if isinstance(response, OAuth2FatalError):
+            raise HTTPBadRequest(reason=str(response)) from response
 
-        url = urlparse(self._request['params']['redirect_uri'])
+        elif isinstance(response, OAuth2RedirectionError):
+            params['error'] = response.error_code
+            params['error_description'] = str(response)
+
+        elif isinstance(response, Exception):
+            logger.error('%s: %s', type(response), response, exc_info=response)
+            raise HTTPInternalServerError() from response
+
+        elif response is None:
+            logger.error('OAUth2 response is not set')
+            raise HTTPInternalServerError()
+
+        else:
+            params.update(asdict(response))
+
+        response_mode = getattr(request, 'response_mode', 'fragment')
+
+        if response_mode == 'query':
+            query.update(params)
+        elif response_mode == 'fragment':
+            fragment.update(params)
+        else:
+            raise HTTPBadRequest(reason='Response mode not supported')
+
+        if request.redirect_uri is None:
+            raise HTTPBadRequest(reason='Redirect uri is not set')
+
+        url = urlparse(request.redirect_uri)
         query.update(parse_qs(url.query))
         fragment.update(parse_qs(url.fragment))
 
-        self.response = Response(
+        return Response(
             status=303,
             headers={
                 'Location':
@@ -79,26 +81,33 @@ class ResultObserver(ObserverBase):
                                 urlencode(fragment, doseq=True)))
             })
 
+    async def on_next(self, response: OAuth2Response) -> None:
+        self._response = response
+
+    async def on_error(self, error: Exception):
+        self._response = error
+
+    async def on_completed(self):
+        logger.debug('Request process finished')
+
 
 class OAuth2Handler(View):
 
-    __endpoint__: Type
+    __endpoint__: Endpoint
 
-    async def handle(self):
-        request = self.request
-        owner = await authenticate_end_user(request)
+    async def handle(self, params: MultiDictProxy) -> Response:
+        owner = await authenticate_end_user(self.request)
 
-        stream = self.__endpoint__(owner=owner, params=request['params'])
+        oauth2_request = OAuth2Request(owner=owner, **params)
+        stream = self.__endpoint__(oauth2_request)
 
-        result = ResultObserver(request)
+        result = ResultObserver(oauth2_request)
         await stream.subscribe(result)
 
-        return result.response
+        return result.http_response()
 
-    async def get(self):
-        self.request['params'] = self.request.query
-        return await self.handle()
+    async def get(self) -> Response:
+        return await self.handle(self.request.query)
 
-    async def post(self):
-        self.request['params'] = await self.request.post()
-        return await self.handle()
+    async def post(self) -> Response:
+        return await self.handle(await self.request.post())
