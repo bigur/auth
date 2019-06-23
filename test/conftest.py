9e246aef177b0e5c7d2639f867336629578314e2
Gennady Kovalev <gik@bigur.ru>
2019-06-23 18:26:39 +0300

    Change fixture dependencies

--- test/conftest.py
+++ test/conftest.py
@@ -1,26 +1,15 @@
 __author__ = 'Gennady Kovalev <gik@bigur.ru>'
-__copyright__ = '(c) 2016-2019 Business group for development management'
+__copyright__ = '(c) 2016-2019 Development management business group'
 __licence__ = 'For license information see LICENSE'
 
-from os import urandom
-from os.path import dirname, normpath
+from logging import getLogger, DEBUG
 
-from aiohttp import CookieJar
-from aiohttp.web import Application
-from aiohttp_jinja2 import setup as jinja_setup
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives.serialization import (Encoding,
-                                                          PublicFormat)
-from jinja2 import FileSystemLoader
-from jwt import decode as jwt_decode
-from kaptan import Kaptan
 from pytest import fixture
 
-from bigur.auth.authn import UserPass
-from bigur.auth.middlewares import session
-from bigur.auth.model import User, Client
-from bigur.auth.store import Memory
+from aiohttp.pytest_plugin import aiohttp_client  # noqa: F401
+
+logger = getLogger(__name__)
+logger.setLevel(DEBUG)
 
 
 # Main loop
@@ -29,40 +18,30 @@ def loop(event_loop):
     return event_loop
 
 
-from aiohttp.pytest_plugin import aiohttp_client  # noqa: F401
-
-
 # Debug
 @fixture
 def debug(caplog):
     '''Enable debug logging in tests.'''
-    from logging import DEBUG
     caplog.set_level(DEBUG, logger='bigur.auth')
 
 
 # Cryptography
-@fixture
+@fixture(scope='module')
 def jwt_key():
+    logger.debug('Generating new JWT key')
+    from cryptography.hazmat.backends import default_backend
+    from cryptography.hazmat.primitives.asymmetric import rsa
     return rsa.generate_private_key(
         public_exponent=65537, key_size=2048, backend=default_backend())
 
 
-# Server
-@fixture
-def app(jwt_key):
-    app = Application(middlewares=[session])
-    app['config'] = Kaptan()
-    app['store'] = Memory()
-    app['jwt_keys'] = [jwt_key]
-    app['cookie_key'] = urandom(32)
-    templates = normpath(dirname(__file__) + '/../templates')
-    jinja_setup(app, loader=FileSystemLoader(templates))
-    return app
-
-
-@fixture
-def authn_userpass(app):
-    app['config'].import_config({
+# App configuration
+@fixture(scope='module')
+def config():
+    logger.debug('Loading configuration')
+    from kaptan import Kaptan
+    config = Kaptan()
+    config.import_config({
         'authn': {
             'cookie': {
                 'secure': False,
@@ -79,39 +58,86 @@ def authn_userpass(app):
             }
         }
     })
+    return config
+
+
+# Store
+@fixture(scope='function')
+def store():
+    logger.debug('Creating store')
+    from bigur.auth.store import Memory
+    return Memory()
+
+
+# Cookie key
+@fixture(scope='module')
+def cookie_key():
+    logger.debug('Generating cookie key')
+    from os import urandom
+    return urandom(32)
+
+
+# Server
+@fixture(scope='function')
+def app(config, store, jwt_key, cookie_key):
+    logger.debug('Creating application')
+    from os.path import dirname, normpath
+    from aiohttp.web import Application
+    from aiohttp_jinja2 import setup as jinja_setup
+    from jinja2 import FileSystemLoader
+    from bigur.auth.middlewares import session
+    app = Application(middlewares=[session])
+    app['config'] = config
+    app['store'] = store
+    app['jwt_keys'] = [jwt_key]
+    app['cookie_key'] = cookie_key
+    templates = normpath(dirname(__file__) + '/../templates')
+    jinja_setup(app, loader=FileSystemLoader(templates))
+    return app
+
+
+@fixture(scope='function')
+def authn_userpass(app):
+    logger.debug('Add userpass authn route')
+    from bigur.auth.authn import UserPass
     return app.router.add_route('*', '/auth/login', UserPass)
 
 
 # Entities
-@fixture
-async def user(app):
-    if 'user' not in app:
-        app['user'] = await app['store'].users.put(User('admin', '123'))
-    yield app['user']
+@fixture(scope='function')
+async def user(store):
+    logger.debug('Creating new user')
+    from bigur.auth.model import User
+    yield await store.users.put(User('admin', '123'))
 
 
-@fixture
-async def client(app, user):
-    if 'client' not in app:
-        app['client'] = await app['store'].clients.put(
-            Client('Test web client', user.id, 'password'))
-    yield app['client']
+@fixture(scope='function')
+async def client(store, user):
+    logger.debug('Creating client')
+    from bigur.auth.model import Client
+    yield await store.clients.put(
+        Client('Test web client', user.id, 'password'))
 
 
 # Client
-@fixture
+@fixture(scope='function')
 async def cookie_jar():
+    logger.debug('Creating CookieJar')
+    from aiohttp import CookieJar
+
     yield CookieJar(unsafe=True)
 
 
-@fixture  # noqa: F811
+@fixture(scope='function')  # noqa: F811
 def cli(loop, app, cookie_jar, aiohttp_client):
     return loop.run_until_complete(aiohttp_client(app, cookie_jar=cookie_jar))
 
 
-@fixture
-def decode_token(app):
-    jwt_key = app['jwt_keys'][0]
+@fixture(scope='module')
+def decode_token(jwt_key):
+    from cryptography.hazmat.primitives.serialization import (Encoding,
+                                                              PublicFormat)
+    from jwt import decode as jwt_decode
     public_bytes = jwt_key.public_key().public_bytes(
         encoding=Encoding.PEM, format=PublicFormat.SubjectPublicKeyInfo)
 
@@ -122,8 +148,9 @@ def decode_token(app):
 
 
 # Queries
-@fixture
+@fixture(scope='function')
 async def login(cli, user):
+    logger.debug('Logining to server with username & password')
     response = await cli.post(
         '/auth/login',
         data={
