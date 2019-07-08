e392a04a839b67e7e0d0dfc24fd21c2516830865
Gennady Kovalev <gik@bigur.ru>
2019-07-08 15:18:13 +0300

    Ignore flake8 warnings

--- bigur/auth/oidc/endpoint/__init__.py
+++ bigur/auth/oidc/endpoint/__init__.py
@@ -2,4 +2,6 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Business group for development management'
 __licence__ = 'For license information see LICENSE'
 
+# flake8: noqa
+
 from .authorization import AuthorizationEndpoint
