033348a7f10e74951c7fb27b4da9f37963f8d546
Gennady Kovalev <gik@bigur.ru>
2019-07-09 17:55:07 +0300

    Fix file header

--- bigur/auth/oauth2/grant/implicit.py
+++ bigur/auth/oauth2/grant/implicit.py
@@ -1,5 +1,5 @@
 __author__ = 'Gennady Kovalev <gik@bigur.ru>'
-__copyright__ = '(c) 2016-2019 Business group for development management'
+__copyright__ = '(c) 2016-2019 Development management business group'
 __licence__ = 'For license information see LICENSE'
 
 from dataclasses import dataclass
