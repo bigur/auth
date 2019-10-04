a2f383ef984fb6c65ff4dced9f06e17126459eba
Gennady Kovalev <gik@bigur.ru>
2019-10-04 19:57:14 +0300

    Move configuration to separate module

---
+++ bigur/auth/config.py
@@ -0,0 +1,7 @@
+__author__ = 'Gennady Kovalev <gik@bigur.ru>'
+__copyright__ = '(c) 2016-2019 Development management business group'
+__licence__ = 'For license information see LICENSE'
+
+from kaptan import Kaptan
+
+config = Kaptan(handler='yaml')
