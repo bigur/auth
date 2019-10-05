ed405b88f505d8b532fc0a057e8a802822981311
Gennady Kovalev <gik@bigur.ru>
2019-10-05 23:28:13 +0300

    Remove old tests

--- tests/test_oauth2_token_endpoint.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'Gennady Kovalev <gik@bigur.ru>'
-__copyright__ = '(c) 2016-2019 Development management business group'
-__licence__ = 'For license information see LICENSE'
-
-from pytest import fixture, mark
-
-
-class TestTokenEndpoint(object):
-    pass
