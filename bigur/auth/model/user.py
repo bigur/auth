b82b7bbaf900c560aa29a97da3576fa6f3a675f0
Gennady Kovalev <gik@bigur.ru>
2019-10-06 09:48:25 +0300

    Adapr user's model to password mixin

--- bigur/auth/model/user.py
+++ bigur/auth/model/user.py
@@ -2,7 +2,7 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Development management business group'
 __licence__ = 'For license information see LICENSE'
 
-from dataclasses import InitVar, dataclass, field
+from dataclasses import dataclass
 from logging import getLogger
 from typing import Dict, Optional
 
@@ -14,19 +14,35 @@ logger = getLogger(__name__)
 
 @dataclass
 class UserMixin(Object):
+    '''Base class for :class:`~bigur.auth.model.user.User`.'''
+    #: User\'s login.
     username: str
+
+    #: User\'s external OpenID connect accounts. Is is a :class:`dict`
+    #: with :class:`~bigur.auth.model.provider.Provider`.id as key
+    #: and OpenID connect provider's user id (subject field of token) as
+    #: value.
     oidc_accounts: Optional[Dict[str, str]] = None
 
 
 @dataclass
 class User(PasswordMixin, UserMixin, AbstractUser):
 
-    def add_oidc_account(self, provider_id: str, subject: str):
+    def add_oidc_account(self, provider_id: str, subject: str) -> None:
+        '''Add external OpenID connect account to user's record.
+
+        :param str provider_id: id of
+            :class:`~bigur.auth.model.provider.Provider`
+        :param str subject: OpenID connect's user id'''
         if self.oidc_accounts is None:
             self.oidc_accounts = {}
         self.oidc_accounts[provider_id] = subject
 
-    def delete_oidc_account(self, provider_id: str):
+    def delete_oidc_account(self, provider_id: str) -> None:
+        '''Delete external OpenID connect account to user's record.
+
+        :param str provider_id: id of
+            :class:`~bigur.auth.model.provider.Provider`'''
         if self.oidc_accounts:
             try:
                 del self.oidc_accounts[provider_id]
--- tests/test_model_user.py
+++ tests/test_model_user.py
@@ -2,7 +2,9 @@ __author__ = 'Gennady Kovalev <gik@bigur.ru>'
 __copyright__ = '(c) 2016-2019 Development management business group'
 __licence__ = 'For license information see LICENSE'
 
-from pytest import fixture, mark
+from pytest import mark
+
+from bigur.auth.model import User, Human
 
 
 class TestModelUser(object):
@@ -10,29 +12,29 @@ class TestModelUser(object):
 
     @mark.asyncio
     async def test_create(self):
-        raise NotImplementedError
+        User(username='admin', password='123')
 
     @mark.asyncio
     async def test_password_init(self):
-        user = User('admin', '123')
+        user = User(username='admin', password='123')
         assert user.verify_password('123')
 
     @mark.asyncio
     async def test_set_password(self):
-        user = User('admin')
+        user = User(username='admin')
         user.set_password('1234')
         assert user.verify_password('1234')
 
     @mark.asyncio
     async def test_invalid_password(self):
-        user = User('admin', '123')
+        user = User(username='admin', password='123')
         assert not user.verify_password('1234')
 
     @mark.asyncio
     async def test_create_human(self):
         Human(
-            'admin',
-            '123',
+            username='admin',
+            password='123',
             given_name='Ivan',
             patronymic='Ivanovich',
             family_name='Ivanov')
